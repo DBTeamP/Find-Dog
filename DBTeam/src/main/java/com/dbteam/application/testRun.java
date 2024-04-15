a8acba36246032679b77adcd25ba3edf6f3c0a39
a75372656 <a75372656@gmail.com>
2024-04-15 13:59:11 +0900

    입양일지 search 수정

--- DBTeam/src/main/java/com/dbteam/application/testRun.java
+++ DBTeam/src/main/java/com/dbteam/application/testRun.java
@@ -1,5 +1,7 @@
 package com.dbteam.application;
 
+import com.dbteam.xml.adoption.AdoptionController;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -65,15 +67,20 @@ public class testRun {
         Scanner sc = new Scanner(System.in);
         do{
             System.out.println("======= FINDING DOG ADOPTION =======");
-            System.out.println("1. 일지 내용 구경하기 ");
-            System.out.println("2. 일지 추가하기");
-            System.out.println("3. 이전 화면으로 이동");
+            System.out.println("1. 입양일지 전체 조회 ");
+            System.out.println("2. 입양 일지 추가하기");
+            System.out.println("3. 입양 일지 삭제하기");
+            System.out.println("4. 제목으로 일지 검색");
+            System.out.println("5. 이전 화면으로 이동");
             System.out.print("번호를 입력해주세요 : ");
             int no = sc.nextInt();
             switch (no) {
                 case 1: break;
                 case 2: break;
                 case 3: return;
+                case 4: return;
+                case 5: return;
+
             }
         }while(true);
     }
@@ -166,7 +173,7 @@ public class testRun {
             int no = sc.nextInt();
 
             switch (no){
-//                case 1: menuController.registMenu(inputVol()); break;
+ //              case 1: menuController.registMenu(inputVol()); break;
                 case 2: break;
 //                case 3: menuController.deleteVol(inputVolNum()); break;
                 case 4: break;
